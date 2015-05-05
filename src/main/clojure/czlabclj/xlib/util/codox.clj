46bb9eb1b0a942d78ac028f4fad2eced8200bea3
Ken Leung <mailtokenl@gmail.com>
2015-05-05 15:56:51 -0700

    kl

---
+++ src/main/clojure/czlabclj/xlib/util/codox.clj
@@ -0,0 +1,44 @@
+;; This library is distributed in  the hope that it will be useful but without
+;; any  warranty; without  even  the  implied  warranty of  merchantability or
+;; fitness for a particular purpose.
+;; The use and distribution terms for this software are covered by the Eclipse
+;; Public License 1.0  (http://opensource.org/licenses/eclipse-1.0.php)  which
+;; can be found in the file epl-v10.html at the root of this distribution.
+;; By using this software in any  fashion, you are agreeing to be bound by the
+;; terms of this license. You  must not remove this notice, or any other, from
+;; this software.
+;; Copyright (c) 2013, Ken Leung. All rights reserved.
+
+(ns ^{:doc ""
+      :author "kenl" }
+
+  czlabclj.xlib.util.codox
+
+  (:gen-class)
+
+  (:require [codox.main :refer :all]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; codox gives a bunch of reflection warnings.  I don't like to see them.
+(set! *warn-on-reflection* false)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+(defn -main "Main Entry"
+
+  [& args]
+
+  (require 'codox.main)
+  (if (> (count args) 0)
+    (let [options {:output-dir (nth args 2)
+                   :root (nth args 0)
+                   :sources [ (nth args 1)]
+                   }]
+      (generate-docs options))
+    (println "usage: codox <src-dir> <out-dir>")))
+
+(set! *warn-on-reflection* true)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+(def ^:private codox-eof nil)
+
