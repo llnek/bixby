2da3ccbb59c6d56092e827d55061efec3bcbea14
Ken Leung <mailtokenl@gmail.com>
2015-07-21 22:55:43 -0700

    kl

---
+++ src/main/clojure/czlabclj/xlib/netty/core.clj
@@ -0,0 +1,52 @@
+;; This library is distributed in  the hope that it will be useful but without
+;; any  warranty; without  even  the  implied  warranty of  merchantability or
+;; fitness for a particular purpose.
+;; The use and distribution terms for this software are covered by the Eclipse
+;; Public License 1.0  (http://opensource.org/licenses/eclipse-1.0.php)  which
+;; can be found in the file epl-v10.html at the root of this distribution.
+;; By using this software in any  fashion, you are agreeing to be bound by the
+;; terms of this license. You  must not remove this notice, or any other, from
+;; this software.
+;; Copyright (c) 2013-2015, Ken Leung. All rights reserved.
+
+(ns ^{:doc ""
+      :author "kenl" }
+
+  czlabclj.xlib.netty.core
+
+  (:require [czlabclj.xlib.util.core
+             :refer
+             [ ]]
+            [czlabclj.xlib.util.str
+             :refer
+             [lcase ucase strim nsb hgl?]])
+
+  (:require [clojure.tools.logging :as log]
+            [clojure.string :as cs])
+
+  (:import [io.netty.util ReferenceCounted]
+
+
+
+           ))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;(set! *warn-on-reflection* false)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+(defn- dbgRefCount ""
+  [obj]
+  (when (instance? ReferenceCounted obj)
+    (log/debug "Object {}: has ref-count = {}"
+               (try (.toString obj)
+                    (catch Throwable _ "???"))
+               (.refCnt obj))))
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;EOF
