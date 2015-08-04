0da00139f1cf85ae2efc2fccd011dffb8172a14f
Ken Leung <mailtokenl@gmail.com>
2015-08-04 14:25:54 -0700

    kl

---
+++ src/main/java/com/zotohlab/skaro/core/Muble.java
@@ -0,0 +1,27 @@
+// This library is distributed in  the hope that it will be useful but without
+// any  warranty; without  even  the  implied  warranty of  merchantability or
+// fitness for a particular purpose.
+// The use and distribution terms for this software are covered by the Eclipse
+// Public License 1.0  (http://opensource.org/licenses/eclipse-1.0.php)  which
+// can be found in the file epl-v10.html at the root of this distribution.
+// By using this software in any  fashion, you are agreeing to be bound by the
+// terms of this license. You  must not remove this notice, or any other, from
+// this software.
+// Copyright (c) 2013-2015, Ken Leung. All rights reserved.
+
+package com.zotohlab.skaro.core;
+
+import com.zotohlab.frwk.core.Gettable;
+import com.zotohlab.frwk.core.Settable;
+
+/**
+ * @author kenl
+ */
+public interface Muble extends Gettable, Settable {
+
+  public Iterable<?> seq();
+  public Object toEDN();
+  public void clear();
+
+}
+
