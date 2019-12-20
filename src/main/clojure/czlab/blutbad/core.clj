3693ab9304c560a6179b17b0e97a55ecc74d0399
Kenneth Leung <ccllnekk@gmail.com>
2019-12-19 20:12:22 -0800

    kl

--- src/main/clojure/czlab/blutbad/core.clj
+++ src/main/clojure/czlab/blutbad/core.clj
@@ -383,11 +383,8 @@
          (c/debug "plug = %s, fn = %s." (c/id plug) f)
          (if-not (fn? f)
            (err plug evt)
-           (let [f' #(try (f %1)
-                          (catch Throwable _
-                            (c/exception _)))]
-             (p/run* sc f' [evt])
-             (c/debug "dispatched %s => %s." (c/id evt) (c/id plug)))))))))
+           (do (p/run* sc f [evt])
+               (c/debug "dispatched %s => %s." (c/id evt) (c/id plug)))))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;EOF
